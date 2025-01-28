5b50f66f720a23be21e31a0c16fdfafb3cecb9e4
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2025-01-28 08:36:08 -0600

    Creacion de la calculadora

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ugb.miprimeraaplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tempval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempval = findViewById(R.id.txtNum1);
+                double num1 = Double.parseDouble(tempval.getText().toString());
+                tempval = findViewById(R.id.txtNum2);
+                double num2 = Double.parseDouble(tempval.getText().toString());
+                double respuesta = num1 + num2;
+                tempval = findViewById(R.id.lblRespuesta);
+                tempval.setText("Respuesta: "+ respuesta);
+
+            }
         });
     }
 }

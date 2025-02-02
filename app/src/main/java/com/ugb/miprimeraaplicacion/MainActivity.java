083e437009f626bf388a3555d0111da1acec328e
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2025-02-02 16:11:28 -0600

    Creacion de la calculadora Suma Resta MUltiplicacion y divición

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ugb.miprimeraaplicacion;
 
+import static com.ugb.miprimeraaplicacion.R.id.txtNum1;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,28 +16,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
+
+public class MainActivity extends AppCompatActivity {
     Button btn;
-    TextView tempval;
+    TextView tempVal;
+    RadioGroup rgb;
+    RadioButton opt;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        public void onClick (View v){
+            tempVal = findViewById(R.id.txtNum1);
+            double num1 = Double.parseDouble(tempVal.getText().toString());
+            tempVal = findViewById(R.id.txtNum2);
+            double num2 = Double.parseDouble(tempVal.getText().toString());
+            double respuesta = num1 + num2;
+            respuesta = 0.0;
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tempval = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempval.getText().toString());
-                tempval = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempval.getText().toString());
-                double respuesta = num1 + num2;
-                tempval = findViewById(R.id.lblRespuesta);
-                tempval.setText("Respuesta: "+ respuesta);
 
+            opt = findViewById(R.id.optSuma);
+            if (opt.isChecked()) {
+                respuesta = num1 + num2;
             }
-        });
+            opt = findViewById(R.id.optResta);
+            if (opt.isChecked()) {
+                respuesta = num1 - num2;
+            }
+            opt = findViewById(R.id.optMultiplicacion);
+            if (opt.isChecked()) {
+                respuesta = num1 * num2;
+            }
+            opt = findViewById(R.id.optDivision);
+            if (opt.isChecked()) {
+                respuesta = num1 / num2;
+            }
+            tempVal = findViewById(R.id.lblRespuesta);
+            tempVal.setText("Respuesta: " + respuesta);
+        }
     }
 }
+
